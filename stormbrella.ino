cfbca0e3123c9127c8dc06d2523ce02cc16ba2b9
Josh Lind <jlind@tableausoftware.com>
2016-07-11 02:34:23 -0700

    simultaneous test strand

--- stormbrella.ino
+++ stormbrella.ino
@@ -2,21 +2,33 @@
  * Stormbrella
  */
 
+#include <Adafruit_NeoPixel.h>
+
 // Hardware.
 unsigned int RELAYS[][2] = {
-  {6, 0},
-  {7, 0},
-  {8, 0},
-  {9, 0},
-};
-int array_size = 4;
+               {6, 0},
+               {7, 0},
+               {8, 0},
+               {9, 0},
+             },
+             array_size = 4;
+
+// Rain.
+const int RAIN_PIN = 3,
+          NUM_LIGHTS = 35;
+
+unsigned int COLOR_CYCLE = 0;
+
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(NUM_LIGHTS, RAIN_PIN, NEO_GRB + NEO_KHZ800);
 
 // Animation.
-const unsigned int DELAY = 50;
-const unsigned int LIGHTNING_CHANCE = 2; // Out of 10000.
-const unsigned int LIGHTNING_CHANCE_BOOST = 250; // Increase while active.
-boolean LIGHTNING_ACTIVE = 0;
-boolean LIGHTNING_CALM = 1;
+const unsigned int DELAY = 50,
+                   LIGHTNING_CHANCE = 10, // Out of 10000.
+                   LIGHTNING_CHANCE_BOOST = 250; // Increase while active.
+
+boolean LIGHTNING_ACTIVE = 0,
+        LIGHTNING_CALM = 1,
+        DEBUG = true;
 
 /**
  * Startup.
@@ -24,10 +36,19 @@ boolean LIGHTNING_CALM = 1;
 void setup () {
   randomSeed(analogRead(0));
 
-  // Initialise the Arduino data pins for output.
-  for (int i = 1; i < array_size; i++) {
+  // Rain.
+  strip1.begin();
+  strip1.show();
+  pinMode(RAIN_PIN, OUTPUT);
+
+  // Initialize lightning pins for output.
+  for (int i = 0; i < array_size; i++) {
     pinMode(RELAYS[i][0], OUTPUT);
     digitalWrite(RELAYS[i][0], LOW);    
+  }
+
+  if (DEBUG) {
+    Serial.begin(9600);
   }
 }
 
@@ -36,8 +57,27 @@ void setup () {
  */
 void loop () {
   lightning();
-
+  rainbowCycle();
   delay(DELAY);
+}
+
+
+/**
+ * Rainbow test pattern.
+ */
+void rainbowCycle () {
+  uint16_t i, j;
+
+  if (COLOR_CYCLE > 256) {
+    COLOR_CYCLE = 0;
+  }
+
+  for (i = 0; i < strip1.numPixels(); i++) {
+    strip1.setPixelColor(i, Wheel(((i * 256 / strip1.numPixels()) + COLOR_CYCLE) & 255));
+  }
+  strip1.show();
+  
+  COLOR_CYCLE++;
 }
 
 
@@ -48,9 +88,10 @@ void lightning () {
   int chance = LIGHTNING_CHANCE;
   boolean anyOn = false;
 
-  // Turn off all lights.
+  // All bulbs.
   for (int i = 0; i < array_size; i++) {
-    digitalWrite(RELAYS[i][0], HIGH);
+    // Turn off first.
+    digitalWrite(RELAYS[i][0], LOW);
 
     // Increase chances for clusters.
     if (LIGHTNING_ACTIVE) {
@@ -63,15 +104,38 @@ void lightning () {
     // Determine if flashing.
     if (random(0, 1000) < chance) {
       // Pick a bulb.
-      int bulb = random(1, array_size);
+      int bulb = random(0, array_size);
       // Switch it on and keep track.
-      digitalWrite(RELAYS[bulb][0], LOW);
+      digitalWrite(RELAYS[bulb][0], HIGH);
       LIGHTNING_ACTIVE = 1;
       anyOn = true;
+
+      if (DEBUG) {
+        Serial.print(bulb);
+        Serial.print(" - ");
+        Serial.println(chance);
+      }
     }
   }
   
   if (!anyOn) {
     LIGHTNING_ACTIVE = 0;
   }  
+}
+
+/**
+ * Get color from 0 to 255 input.
+ */
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 85) {
+   return strip1.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return strip1.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  else {
+   WheelPos -= 170;
+   return strip1.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
